ab369790e3edce935b370cb5861ab38b79e42be7
jmelton22 <jmelton22@gmail.com>
2020-03-30 12:32:28 -0400

    Added person-level data query and data cleaning

---
+++ persons/data_prep.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+
+def main():
+    path = '../data/persons'
+    data_2015 = pd.read_csv(f'{path}/person_data_2015.csv', header=0)
+    data_2016 = pd.read_csv(f'{path}/person_data_2016.csv', header=0)
+
+    data_2015 = data_prep(data_2015)
+    data_2016 = data_prep(data_2016)
+
+    accident_data = pd.concat([data_2015, data_2016])
+    accident_data.reset_index(drop=True, inplace=True)
+
+    accident_data.to_csv(f'{path}/person_data_clean.csv', index=False)
+
+
+def data_prep(data):
+
+    def combine_safety(x, y):
+        if x == 'Not a Motor Vehicle Occupant':
+            return y
+        else:
+            return x
+
+    data['safety_equipment_use'] = data.apply(lambda row: combine_safety(row['safety_equipment_use'], row['non_motorist_safety_equipment_use']), axis=1)
+    data.drop('non_motorist_safety_equipment_use', axis=1, inplace=True)
+
+    data.replace({'Unknown': np.nan,
+                  'Not Reported': np.nan,
+                  'Trafficway Not in State Inventory': np.nan,
+                  'Unknown Body Type': np.nan,
+                  'Unknown if Ejected (Since 2009)': np.nan,
+                  'Unknown if Used': np.nan,
+                  'Unknown if Helmet Worn': np.nan,
+                  'Deployment Unknown': np.nan,
+                  'Unknown Location': np.nan,
+                  '': np.nan}, inplace=True)
+
+    data['age'].replace({998: np.nan,
+                         999: np.nan}, inplace=True)
+
+    data.dropna(inplace=True)
+
+    data['fatality'] = data['injury_severity'].apply(lambda x: 1 if 'Fatal' in x or 'Died' in x else 0)
+
+    # Refactor land_use column to binary (0: Urban; 1: Rural)
+    data['land_use'].replace({'Urban': 1,
+                              'Rural': 0},
+                             inplace=True)
+    data.rename(columns={'land_use': 'land_use_urban'}, inplace=True)
+
+    data['rollover'] = data['rollover'].apply(lambda x: 0 if x == 'No Rollover' else 1)
+
+    def airbag_bin(x):
+        if x == 'Not Deployed':
+            return 0
+        elif x == 'Not Applicable':
+            return -1
+        else:
+            return 1
+
+    data['air_bag_deployed'] = data['air_bag_deployed'].apply(airbag_bin)
+
+    data['age'] = data['age'].astype(int)
+
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+
+if __name__ == '__main__':
+    main()
---
+++ persons/query.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from google.cloud import bigquery
+from bq_helper import BigQueryHelper
+
+
+def main():
+    path = '../data/persons'
+    bq_assistant = BigQueryHelper('bigquery-public-data', 'nhtsa_traffic_fatalities')
+
+    QUERY = """
+        SELECT
+            p.consecutive_number,
+            p.vehicle_number,
+            p.person_number,
+            person_type_name AS person_type,
+            age,
+            sex,
+            injury_severity_name AS injury_severity,
+            land_use_name AS land_use,
+            functional_system_name AS trafficway_type,
+            manner_of_collision_name AS manner_of_collision,
+            body_type_name AS body_type,
+            rollover,
+            seating_position_name AS seating_position,
+            ejection_name AS ejection,
+            restraint_system_helmet_use_name AS safety_equipment_use,
+            air_bag_deployed_name AS air_bag_deployed,
+            non_motorist_safety_equipment_use
+        FROM
+            `bigquery-public-data.nhtsa_traffic_fatalities.person_{0}` AS p
+        LEFT JOIN
+            `bigquery-public-data.nhtsa_traffic_fatalities.safetyeq_{0}` AS s
+            ON p.consecutive_number=s.consecutive_number AND p.vehicle_number=s.vehicle_number AND p.person_number=s.person_number
+        ORDER BY
+            consecutive_number, vehicle_number, person_number
+    """
+
+    data_2015 = bq_assistant.query_to_pandas(QUERY.format(2015))
+    data_2016 = bq_assistant.query_to_pandas(QUERY.format(2016))
+
+    data_2015.to_csv(f'{path}/person_data_2015.csv', index=False)
+    data_2016.to_csv(f'{path}/person_data_2016.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
